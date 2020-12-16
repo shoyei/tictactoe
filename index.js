ae0013532c967bba6b999b9fa3a13954158a7d69
shoyei <zacsrogers@gmail.com>
2020-12-15 17:09:25 -0800

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //Gameboard module stores player moves in an array
-const gameBoardMod = (() => {
+const interfaceMod = (() => {
     let gameArray = ["", "", "", "", "", "", "", "", ""]
     let gameStart = false;
     let p1Turn = true;
@@ -10,7 +10,6 @@ const gameBoardMod = (() => {
 
     //DOM selectors
     const gameContainer = document.querySelector('#container');
-    const startInput = document.querySelector('#start');
     const p1NameInput = document.querySelector('#p1Name');
     const p1CharInput = document.querySelector('#p1Char');
     const p2NameInput = document.querySelector("#p2Name");
@@ -18,19 +17,25 @@ const gameBoardMod = (() => {
     const p1Name = document.querySelector('#player1Name')
     const p2Name = document.querySelector('#player2Name')
     const overlay = document.querySelector('#overlay');
+    const overlay2 = document.querySelector('#overlay2');
     const okButton = document.querySelector('#okButton');
     const p1Char = document.querySelector('#player1Char');
     const p2Char = document.querySelector('#player2Char');
     const resetBtn = document.querySelector('#resetBtn');
+    const announce = document.querySelector('#announcement');
+    const exit = document.querySelector("#exit")
+    let startInput = document.querySelector('#start');
 
     //Start Game button brings up overlay for inputting player details
     function startGame() {
         startInput.addEventListener('click', function () {
             overlay.style.display = 'flex';
+            if (gameStart == true) startInput.disabled = true
+
         });
     }
 
-    //Character button on overlay switches player characters.
+    //Character button in overlay form switches player characters.
     p1CharInput.addEventListener('click', charSwap, false);
     p2CharInput.addEventListener('click', charSwap, false);
 
@@ -49,16 +54,29 @@ const gameBoardMod = (() => {
         }
     }
 
-    //Reset button
+    //Reset buttons
     resetBtn.addEventListener('click', resetBoard, false)
+    exit.addEventListener('click', resetBoard, false)
+
     function resetBoard() {
+        overlay2.style.display = 'none';
         gameStart = false;
         p1Turn = true;
+        turnsLeft = 9;
         p1NameInput.value = "";
         p2NameInput.value = "";
+        p1Name.textContent = 'Player 1';
+        p2Name.textContent = 'Player 2';
+        p1Char.textContent = '';
+        p2Char.textContent = '';
+        for (i = 0; i < interfaceMod.gameArray.length; i++) {
+            interfaceMod.gameArray[i] = "";
+        }
+        toDisplay()
+        startInput.textContent = `Start Game`;
     }
 
-    //Clear and display gameArray's values to the DOM
+    //Initialize and display gameArray's values to the DOM
     function toDisplay() {
         while (gameContainer.firstChild) {
             gameContainer.removeChild(gameContainer.lastChild);
@@ -75,16 +93,17 @@ const gameBoardMod = (() => {
     //Clicking on "let's go" creates the player objects based on the inputs and starts turn #1
     function playTime() {
         okButton.addEventListener('click', function () {
-            gameBoardMod.player1 = playerFact(p1NameInput.value, 1, p1CharInput.value);
-            gameBoardMod.player2 = playerFact(p2NameInput.value, 2, p2CharInput.value);
-            p1Name.textContent = `${gameBoardMod.player1.name}`;
-            p2Name.textContent = `${gameBoardMod.player2.name}`;
+            interfaceMod.player1 = playerFact(p1NameInput.value, 1, p1CharInput.value);
+            interfaceMod.player2 = playerFact(p2NameInput.value, 2, p2CharInput.value);
+            p1Name.textContent = `${interfaceMod.player1.name}`;
+            p2Name.textContent = `${interfaceMod.player2.name}`;
             p1Char.textContent = `${p1CharInput.value.toUpperCase()}`;
             p2Char.textContent = `${p2CharInput.value.toUpperCase()}`;
             overlay.style.display = 'none';
             gameStart = true;
-            startInput.textContent = `${gameBoardMod.player1.name}'s turn`;
+            startInput.textContent = `${interfaceMod.player1.name}'s turn`;
         });
+        
     }
 
     //Places the current player's character into the gameArray and displays the results in the DOM
@@ -94,23 +113,23 @@ const gameBoardMod = (() => {
         let _boxID = e.target.getAttribute('id');
         if (!gameStart) return;
         if (gameArray[_boxID] !== "") return;
-        if (p1Turn) {
-            gameArray[_boxID] = gameBoardMod.player1.character;
-            startInput.textContent = `${gameBoardMod.player2.name}'s turn`;
-        } else if (!p1Turn) {
-            gameArray[_boxID] = `${gameBoardMod.player2.character}`;
-            startInput.textContent = `${gameBoardMod.player1.name}'s turn`;
+        if (turnsLeft % 2 === 1) {
+            gameArray[_boxID] = interfaceMod.player1.character;
+            startInput.textContent = `${interfaceMod.player2.name}'s turn`;
+        } else if (turnsLeft % 2 === 0) {
+            gameArray[_boxID] = `${interfaceMod.player2.character}`;
+            startInput.textContent = `${interfaceMod.player1.name}'s turn`;
         }
         toDisplay();
         turnsLeft -= 1;
         if (turnsLeft < 5 && checkWin() && p1Turn) {
-            p1wins();
+            wins(`${interfaceMod.player1.name} wins!`);
         }
         if (turnsLeft < 5 && checkWin() && !p1Turn) {
-            p2wins();
+            wins(`${interfaceMod.player2.name} wins!`);
         }
         else if (turnsLeft == 0 && !checkWin()) {
-            draw();
+            wins('Draw');
         }
         p1Turn = !p1Turn;
     };
@@ -137,9 +156,15 @@ const gameBoardMod = (() => {
         return false
     }
 
+    function wins(_string) {
+        overlay2.style.display = 'flex';
+        announce.textContent = _string;
+    }
+
     return {
         p1Turn,
         gameArray,
+        gameStart: gameStart,
         display: toDisplay(),
         playTime: playTime(),
         startGame: startGame(),
@@ -149,9 +174,9 @@ const gameBoardMod = (() => {
     }
 })();
 
-const interfaceMod = (() => {
-    
-})(;)
+const gameMod = (() => {
+
+})();
 
 //Factory function for creating player object
 const playerFact = (name, playerID, symbol) => {
